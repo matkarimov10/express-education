4f1fffb357d77de84c6a82629fb7a9992843afbd
matkarimov10 <matkarimov1099@gmail.com>
2024-02-02 18:14:49 +0500

    fix:supabase url

--- src/client/supabase.js
+++ src/client/supabase.js
@@ -1,6 +1,6 @@
 import {createClient} from "@supabase/supabase-js"
 
-const supabaseUrl = import.meta.env.VITE_SUPAURL
-const supabaseKey = import.meta.env.VITE_SUPAKEY
-
+const supabaseUrl = 'https://vpvowchtxaflctpdhfre.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZwdm93Y2h0eGFmbGN0cGRoZnJlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDY4NzkzMjUsImV4cCI6MjAyMjQ1NTMyNX0.O1v5hX4-BxrHjbTEfAS9T2KpCy7DIYnIBlEMlxme0nY'
+//tIfvgmq6uxj7addn
 export const supabase  = createClient(supabaseUrl, supabaseKey)
